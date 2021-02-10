adfe676b5b951a5fd124e465a28c401bdad18f14
sedasimsekk <78797689+sedasimsekk@users.noreply.github.com>
2021-02-10 05:49:14 +0300

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+     class CustomerManager
+    {
+        public  void Add(Customer customer)
+        {
+            Console.WriteLine("--------------------ADD----------------------");
+            Console.WriteLine("Please your name enter");
+            customer.Name = Console.ReadLine();
+            Console.WriteLine("Please your surname enter");
+            customer.Surname = Console.ReadLine();
+            Console.WriteLine("Please your adress enter");
+            customer.Adress = Console.ReadLine();
+            Console.WriteLine("Customer Added!\n" + customer.Name + "\n" + customer.Surname + "\n" + customer.Adress);
+        }
+
+        public void List (Customer[] customers)
+        {
+            Console.WriteLine("---------------LİST-----------------------");
+            foreach (var cust in customers)
+            {
+                
+                Console.WriteLine("ID:"+cust.Id+"\nADİ:"+cust.Name+"\nSOYADİ:"+cust.Surname+"\nADRESS:"+cust.Adress);
+            }
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("--------------DELETE-----------------------");
+            Console.WriteLine(customer.Name+" DELETED!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "Engin";
+            customer.Surname = "Demiroğ";
+            customer.Adress = "İstanbul";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Seda";
+            customer2.Surname = "Şimşek";
+            customer2.Adress = "Diyarbakir";
+
+            Customer[] customers = new Customer[] { customer, customer2 };
+            CustomerManager customerManager = new CustomerManager();
+            Customer customer3 = new Customer();//boş bir customer nesnesi add kısmında içini dolduruyoruz
+            customerManager.Add(customer3);
+            customerManager.List(customers);
+            customerManager.Delete(customer);
+
+        }
+    }
+}
+
